30645eb9dd05e720c34edb2fa60e331a2b757071
EugenVolosciuc <eugen.volosciuc@icoldo.com>
2020-11-05 17:29:23 +0200

    Added request for lessons

--- pages/app/schools/[school_id]/calendar.js
+++ pages/app/schools/[school_id]/calendar.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Row, Col } from 'antd'
 import dayjs from 'dayjs'
+import useSWR from 'swr'
 
 import { withAuth } from 'utils/hoc/withAuth'
 import DashboardLayout from 'components/layouts/DashboardLayout/DashboardLayout'
 import LessonCalendar from 'components/cards/LessonCalendar/LessonCalendar'
+import { authContext } from 'utils/hoc/withAuth'
+import fetcher from 'utils/functions/fetcher'
 
 const CalendarPage = () => {
+    const { user, userLoading } = useContext(authContext)
+    const url = '/lessons'
+
+    const { data, error, isValidating, mutate } = useSWR([url], () => fetcher(url, {
+        filters: { 
+            school: user.school,
+            startAt: '2020-10-01',
+            endAt: '2020-11-01'
+        }
+    }))
 
     const handleDaySelect = date => {
         console.log("date", date)
