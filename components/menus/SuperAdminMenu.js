f9e3513c600d5ef70a303326ee786c89a65fbc3b
EugenVolosciuc <eugen.volosciuc@icoldo.com>
2020-11-16 17:02:41 +0200

    Modified Super Admin Menu

--- components/menus/SuperAdminMenu.js
+++ components/menus/SuperAdminMenu.js
@@ -5,17 +5,22 @@ import {
     UserOutlined,
     HomeOutlined,
     DollarCircleOutlined,
-    FieldTimeOutlined
+    FieldTimeOutlined,
+    SettingOutlined
 } from '@ant-design/icons'
 import { useRouter } from 'next/router'
 
 import { Link } from 'i18n'
 
+const subMenuKeys = ['/app/admin/system']
+
 const SuperAdminMenu = () => {
     const router = useRouter()
 
+    const getOpenKeys = () => subMenuKeys.filter(key => router.route.includes(key))
+
     return (
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={[router.route]}>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={[router.route]} defaultOpenKeys={getOpenKeys()}>
             <Menu.Item key="/app/admin" icon={<DashboardOutlined />}>
                 <Link href="/app/admin" as="/app/admin"><a>Dashboard</a></Link>
             </Menu.Item>
@@ -25,12 +30,14 @@ const SuperAdminMenu = () => {
             <Menu.Item key="/app/admin/schools" icon={<HomeOutlined />}>
                 <Link href="/app/admin/schools" as="/app/admin/schools"><a>Schools</a></Link>
             </Menu.Item>
-            <Menu.Item key="/app/admin/system/cron" icon={<FieldTimeOutlined />}>
-                <Link href="/app/admin/system/cron" as="/app/admin/system/cron"><a>Cron jobs</a></Link>
-            </Menu.Item>
             <Menu.Item key="/app/admin/finances" icon={<DollarCircleOutlined />}>
                 <Link href="/app/admin/finances" as="/app/admin/finances"><a>Finances</a></Link>
             </Menu.Item>
+            <Menu.SubMenu key="/app/admin/system" icon={<SettingOutlined />} title="System">
+                <Menu.Item key="/app/admin/system/cron" icon={<FieldTimeOutlined />}>
+                    <Link href="/app/admin/system/cron" as="/app/admin/system/cron"><a>Cron jobs</a></Link>
+                </Menu.Item>
+            </Menu.SubMenu>
         </Menu>
     )
 }
